e4b021d5d6c32c385c1893b8a4453539f1f7985c
MulindwaMarkMyles <mylesmark753@gmail.com>
2024-10-13 09:17:51 +0300

    done

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
-import 'package:hive_flutter/hive_flutter.dart';
-import 'stock.dart'; // Import the Stock model
 
 class Homescreen extends StatefulWidget {
   const Homescreen({super.key});
@@ -13,25 +10,7 @@ class Homescreen extends StatefulWidget {
 
 class _HomescreenState extends State<Homescreen> {
   String? selectedStore;
-  late Future<Box<Stock>>
-      stockBoxFuture; // For storing the future of box opening
-
-  @override
-  void initState() {
-    super.initState();
-    stockBoxFuture =
-        Hive.openBox<Stock>('thestocks'); // Open the box asynchronously
-  }
-
-  List<DropdownMenuItem<String>> get items {
-    return [
-      const DropdownMenuItem(value: "Store 1", child: Text("Store 1")),
-      const DropdownMenuItem(value: "Store 2", child: Text("Store 2")),
-      const DropdownMenuItem(value: "Store 3", child: Text("Store 3")),
-      const DropdownMenuItem(value: "Store 4", child: Text("Store 4")),
-      const DropdownMenuItem(value: "Store 5", child: Text("Store 5")),
-    ];
-  }
+  final List<Stock> stockList = []; // List to store the stock items
 
   @override
   Widget build(BuildContext context) {
@@ -80,114 +59,82 @@ class _HomescreenState extends State<Homescreen> {
           ),
         ),
       ),
-      body: FutureBuilder<Box<Stock>>(
-        future: stockBoxFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-
-          if (snapshot.hasError) {
-            return const Center(child: Text("Error loading stocks."));
-          }
-
-          final stockBox = snapshot.data!; // The box is now available
-
-          return Container(
-            color: Colors.white,
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
               children: [
-                Row(
-                  children: [
-                    Text("Store:", style: GoogleFonts.poppins()),
-                    const SizedBox(width: 16),
-                    DropdownButton<String>(
-                      value: selectedStore,
-                      items: items,
-                      onChanged: (value) {
-                        setState(() {
-                          selectedStore = value;
-                        });
-                      },
-                      hint: const Text("Select Store"),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 16),
-                Expanded(
-                  child: ValueListenableBuilder(
-                    valueListenable: stockBox.listenable(),
-                    builder: (context, Box<Stock> box, _) {
-                      if (box.isEmpty) {
-                        return const Text("No stock available.");
-                      }
-
-                      // Filter the stocks by the selected store
-                      final filteredStocks = box.values.where((stock) {
-                        return selectedStore == null ||
-                            stock.store == selectedStore;
-                      }).toList();
-
-                      if (filteredStocks.isEmpty) {
-                        return const Text(
-                            "No stock available for the selected store.");
-                      }
-
-                      return Table(
-                        border: TableBorder.all(color: Colors.black),
-                        children: [
-                          TableRow(
-                            decoration: const BoxDecoration(
-                              color: Color.fromARGB(85, 223, 222, 222),
-                            ),
-                            children: [
-                              _buildTableCell('Name'),
-                              _buildTableCell('Units'),
-                              _buildTableCell('Unit Price'),
-                              _buildTableCell('Total Price'),
-                            ],
-                          ),
-                          ...filteredStocks.map((stock) {
-                            return _buildDataRow(
-                              stock.name,
-                              stock.units.toString(),
-                              stock.unitPrice.toString(),
-                              stock.totalPrice.toString(),
-                            );
-                          }),
-                        ],
-                      );
-                    },
-                  ),
+                Text("Store:", style: GoogleFonts.poppins()),
+                const SizedBox(width: 16),
+                DropdownButton<String>(
+                  value: selectedStore,
+                  items: items,
+                  onChanged: (value) {
+                    setState(() {
+                      selectedStore = value;
+                    });
+                  },
+                  hint: const Text("Select Store"),
                 ),
               ],
             ),
-          );
-        },
+            const SizedBox(height: 16),
+            Expanded(
+              child: stockList.isEmpty
+                  ? const Center(child: Text("No stock available."))
+                  : ListView.builder(
+                      itemCount: stockList.length,
+                      itemBuilder: (context, index) {
+                        final stock = stockList[index];
+                        if (selectedStore == null ||
+                            stock.store == selectedStore) {
+                          return Card(
+                            child: ListTile(
+                              title: Text(stock.name),
+                              subtitle: Text(
+                                'Units: ${stock.units} | Unit Price: ${stock.unitPrice} UGX',
+                              ),
+                              trailing: Text(
+                                'Total: ${stock.totalPrice.toStringAsFixed(2)} UGX',
+                              ),
+                            ),
+                          );
+                        }
+                        return const SizedBox.shrink();
+                      },
+                    ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () =>
-            _showAddStockDialog(context), // Show the dialog when pressed
+        onPressed: () => _showAddStockDialog(context), // Show the dialog
         child: const Icon(Icons.add),
       ),
     );
   }
 
+  List<DropdownMenuItem<String>> get items {
+    return [
+      const DropdownMenuItem(value: "Store 1", child: Text("Store 1")),
+      const DropdownMenuItem(value: "Store 2", child: Text("Store 2")),
+      const DropdownMenuItem(value: "Store 3", child: Text("Store 3")),
+      const DropdownMenuItem(value: "Store 4", child: Text("Store 4")),
+      const DropdownMenuItem(value: "Store 5", child: Text("Store 5")),
+    ];
+  }
+
   void _showAddStockDialog(BuildContext context) {
-    final TextEditingController nameController = TextEditingController();
-    final TextEditingController unitsController = TextEditingController();
-    final TextEditingController unitPriceController = TextEditingController();
+    final nameController = TextEditingController();
+    final unitsController = TextEditingController();
+    final unitPriceController = TextEditingController();
 
     showDialog(
       context: context,
       builder: (context) {
         return AlertDialog(
           title: const Text('Add Stock'),
-          shape: RoundedRectangleBorder(
-            borderRadius:
-                BorderRadius.circular(8), // Less rounded for boxy look
-          ),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
@@ -209,35 +156,10 @@ class _HomescreenState extends State<Homescreen> {
           ),
           actions: [
             TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.red, // Text color for Cancel button
-                textStyle: TextStyle(
-                  fontSize: 18,
-                  fontFamily: GoogleFonts.poppins().fontFamily,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8), // Boxy button
-                ),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              ),
               onPressed: () => Navigator.of(context).pop(),
               child: const Text('Cancel'),
             ),
             ElevatedButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.blue, // Text color for Cancel button
-                textStyle: TextStyle(
-                  fontSize: 18,
-                  fontFamily: GoogleFonts.poppins().fontFamily,
-                  // fontWeight: FontWeight.bold,
-                ),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8), // Boxy button
-                ),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              ),
               onPressed: () {
                 final name = nameController.text;
                 final units = int.tryParse(unitsController.text) ?? 0;
@@ -246,15 +168,13 @@ class _HomescreenState extends State<Homescreen> {
 
                 if (name.isNotEmpty && units > 0 && unitPrice > 0) {
                   if (selectedStore != null) {
-                    _addStock(
-                        name, units, unitPrice, selectedStore!); // Add stock
+                    _addStock(name, units, unitPrice, selectedStore!);
+                    Navigator.of(context).pop();
                   } else {
-                    // Handle the case when selectedStore is null
                     ScaffoldMessenger.of(context).showSnackBar(
                       const SnackBar(content: Text('Please select a store')),
                     );
                   }
-                  Navigator.of(context).pop(); // Close the dialog
                 }
               },
               child: const Text('Add'),
@@ -265,49 +185,32 @@ class _HomescreenState extends State<Homescreen> {
     );
   }
 
-  void _addStock(String name, int units, double unitPrice, String store) async {
-    final stockBox =
-        await Hive.openBox<Stock>('thestocks'); // Ensure the box is open
-    final stock =
-        Stock(name: name, units: units, unitPrice: unitPrice, store: store);
-    await stockBox.add(stock); // Add stock to Hive
-  }
-
-  TableRow _buildDataRow(
-      String name, String units, String unitPrice, String totalPrice) {
-    return TableRow(
-      children: [
-        _buildDataCell(name),
-        _buildDataCell(units),
-        _buildDataCell(unitPrice),
-        _buildDataCell(totalPrice),
-      ],
+  void _addStock(String name, int units, double unitPrice, String store) {
+    final stock = Stock(
+      name: name,
+      units: units,
+      unitPrice: unitPrice,
+      store: store,
     );
-  }
 
-  Widget _buildTableCell(String text) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(
-        text,
-        style: TextStyle(
-          fontWeight: FontWeight.bold,
-          fontFamily: GoogleFonts.poppins().fontFamily,
-        ),
-      ),
-    );
+    setState(() {
+      stockList.add(stock); // Add stock to the list
+    });
   }
+}
 
-  Widget _buildDataCell(String text) {
-    return Container(
-      padding: const EdgeInsets.all(8.0),
-      child: Text(
-        text,
-        style: GoogleFonts.roboto(
-          fontSize: 25.0,
-          color: Colors.black,
-        ),
-      ),
-    );
-  }
+class Stock {
+  final String name;
+  final int units;
+  final double unitPrice;
+  final String store;
+
+  Stock({
+    required this.name,
+    required this.units,
+    required this.unitPrice,
+    required this.store,
+  });
+
+  double get totalPrice => units * unitPrice;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:stockapp/login.dart';
-import 'dart:io';
-import 'package:stockapp/stock.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  var path = Directory.current.path;
-  Hive.init(path);
-
-  // Register the Stock adapter
-  Hive.registerAdapter(StockAdapter());
-  
-
-
-  // Open a Hive box for stocks
-  await Hive.openBox<Stock>('thestocks');
-  
-
   runApp(TheStockApp());
 }
 
